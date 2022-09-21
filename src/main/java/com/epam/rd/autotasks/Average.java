80005ba9f5ce457b42fe23870f976d252148ae20
Your Namegit config --global user.name Veronika <veronika.tarasova.98@gmail.com>
2022-09-21 15:13:22 +0300

    edited main

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -1,12 +1,37 @@
 package com.epam.rd.autotasks;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Use Scanner methods to read input
+        boolean nextInt = true;
+        int sum = 0;
 
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        while(nextInt){
+            int number;
+            number = scanner.nextInt();
+            if(number != 0){
+                numbers.add(number);
+            }
+            else{
+                nextInt = false;
+            }
+        }
+
+
+        for(Integer item : numbers){
+
+            sum += item;
+        }
+        int average;
+        average = sum / numbers.size();
+
+        System.out.println(average);
     }
 
 }
